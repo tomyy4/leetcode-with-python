32f731595d90e8897d269cdc3886ea460b9f5c26
tomeullompartpiza <tomeullompartpiza@gmail.com>
2026-01-03 19:44:50 +0100

    refactor max_consecutives ones, improved: O(n) time, O(1) space ✅

--- array-101/max_consecutives_ones.py
+++ array-101/max_consecutives_ones.py
@@ -1,5 +1,6 @@
 from typing import List
 
+### FIRST ATTEMPT #####
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
         # will store the sequences
@@ -19,8 +20,31 @@ class Solution:
         return max(seqs)
 
 
+
+
+##### REFACTOR DAY 2 #####
+def max_consecutives_ones(nums):
+    max_count = 0
+    current_seq = 0
+
+    for num in nums:
+        if num == 1:
+            current_seq += 1
+        else:
+            if current_seq > max_count:
+                max_count = current_seq
+            current_seq = 0
+    
+    # in case the 1 sequence ends without a zero and it's greater than current max_count
+    if current_seq > 0 and current_seq > max_count:
+            max_count = current_seq
+    
+    return max_count
+
 nums = [1,1,0,1,1,1]
 nums2 = [1,0,1,1,0,1]
 s = Solution()
 print(s.findMaxConsecutiveOnes(nums=nums))
 print(s.findMaxConsecutiveOnes(nums=nums2))
+print(max_consecutives_ones(nums=nums))
+print(max_consecutives_ones(nums=nums2))
