c7eab170d34cc4dc7e979d254e6a6cf4a378ceb9
tomeullompartpiza <tomeullompartpiza@gmail.com>
2026-01-04 14:52:47 +0100

    refactor squares of sorted array

--- array-101/squares_of_a_sorted_array.py
+++ array-101/squares_of_a_sorted_array.py
@@ -7,6 +7,25 @@ class Solution:
         return sorted([n*n for n in nums])
 
 
+### REFACTOR DAY 3 ######
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        ordered = [n for n in nums]
+        left,right = 0, len(nums) -1
+        pos = len(nums)- 1
+        while left <= right:
+            if abs(nums[right]) > abs(nums[left]):
+                ordered[pos] = nums[right] * nums[right]
+                right -=1
+            else:
+                ordered[pos] = nums[left] * nums[left]
+                left +=1
+            pos -=1
+
+        return ordered
+
+nums_1=[-4,-1,0,3,10]
+nums_2=[-7,-3,2,3,11]
 s = Solution()
-print(s.sortedSquares(nums=[-4,-1,0,3,10]))
-print(s.sortedSquares(nums=[-7,-3,2,3,11]))
+print(s.sortedSquares(nums=nums_1))
+print(s.sortedSquares(nums=nums_2))
