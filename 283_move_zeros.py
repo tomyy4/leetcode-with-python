ba9e2bf68167e65411043932a3e814b908afbb0a
tomeullompartpiza <tomeullompartpiza@gmail.com>
2026-01-08 08:17:41 +0100

    remove comments in 283

--- 283_move_zeros.py
+++ 283_move_zeros.py
@@ -1,29 +1,7 @@
 
+from typing import List
+
 arr = [0,1,0,3,12]
-
-"""
-##### first iteration #####
-- No swaps
-- resulting array = [0,1,0,3,12]
-- slow=0, fast=1
-
-##### second iteration #####
-- resulting array = [1,0,0,3,12]
-- slow=1, fast=2
-
-##### third iteration #####
-- resulting array = [1,0,0,3,12]
-slow=1, fast=3
-
-##### fourth iteration #####
-resulting array = [1,3,0,0,12]
-slow=2, fast=4
-
-##### fifth iteration #####
-resulting array = [1,3,12,0,0]
-slow=3, fast=5
-
-"""
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
