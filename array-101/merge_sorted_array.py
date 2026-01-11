c0eaa64fe21834a7d7a84aa43a2c62d3376e0ea5
tomeullompartpiza <tomeullompartpiza@gmail.com>
2026-01-12 00:10:06 +0100

    merge_sorted_array

---
+++ array-101/merge_sorted_array.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        pass
+
+#print(s.merge(nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3))    
+#print(s.merge(nums1 = [1], m = 1, nums2 = [], n = 0))    
+#print(s.merge(nums1 = [0], m = 0, nums2 = [1], n = 1))    
+#print(s.merge([0],0,[1], 1))
+#print(s.merge([2,0],1, [1],1))
+s = Solution()
+print(s.merge(nums1=[3, 0, 0], m=1, nums2=[1, 2], n=2))
